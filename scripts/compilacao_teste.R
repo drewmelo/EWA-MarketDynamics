937fd9c2499deea5d1247ac554478d276c11671a
André <andremelopix@hotmail.com>
2026-05-02 11:02:15 -0300

    refactor: melhorar robustez das simulações com tratamento de erros e controle de sink

--- scripts/compilacao_teste.R
+++ scripts/compilacao_teste.R
@@ -8,28 +8,55 @@
 lista_dfs <- base::list()
 
 # Verificando o resultado (100 vezes)
-for (i in 1:100) {
+viewer_antigo <- getOption("viewer")
+options(viewer = function(url, height = NULL) invisible(NULL))
+
+lista_dfs <- base::list()
+
+n_iter <- 100
+pb <- utils::txtProgressBar(min = 0, max = n_iter, style = 3)
+
+for (i in seq_len(n_iter)) {
+  
   matriz_1_ex <- rgamer::normal_form(
     players = c("Vendedor", "Comprador"),
     pars = c("preco_vendedor", "estrategia_comprador"),
-    s1 = c("Preço Alto", "Preço Baixo"),  # Estratégias do vendedor
-    s2 = c("Aceitar", "Rejeitar"),  # Estratégias do comprador
+    s1 = c("Preço Alto", "Preço Baixo"),
+    s2 = c("Aceitar", "Rejeitar"),
     payoffs1 = func_payoff1,
     payoffs2 = func_payoff2,
-    discretize = T
+    discretize = TRUE
   )
-
-  resultado <- rgamer::solve_nfg(matriz_1_ex, mark_br = T)
-
-  # Extrair a primeira linha do resultado
+  
+  tmp <- tempfile()
+  con <- file(tmp, open = "wt")
+  
+  resultado <- tryCatch({
+    sink(con, type = "output")
+    sink(con, type = "message")
+    
+    rgamer::solve_nfg(matriz_1_ex, mark_br = TRUE)
+    
+  }, finally = {
+    if (sink.number(type = "message") > 0) sink(type = "message")
+    if (sink.number(type = "output") > 0) sink(type = "output")
+    close(con)
+    unlink(tmp)
+  })
+  
   if (base::is.character(resultado)) {
     df_resultado <- tibble::tibble(NE = resultado)
   } else {
     df_resultado <- tibble::tibble(NE = resultado[1])
   }
-
+  
   lista_dfs[[i]] <- df_resultado
+  
+  utils::setTxtProgressBar(pb, i)
 }
+
+close(pb)
+options(viewer = viewer_antigo)
 
 ne_m1 <- base::do.call(rbind, lista_dfs)
 
@@ -43,7 +70,7 @@ ne_m1 <- ne_m1 |>
   dplyr::ungroup()
 
 # Criando o gráfico com ggplot2
-p22 <- ggplot2::ggplot(ne_m1, ggplot2::aes(x = count_n, color = NE, group = NE)) +
+p19 <- ggplot2::ggplot(ne_m1, ggplot2::aes(x = count_n, color = NE, group = NE)) +
   ggplot2::geom_freqpoly(binwidth = 3.7, linewidth = 1.6, position = 'jitter') +
   ggplot2::labs(
     # title = "Contagem Acumulada por Categoria de NE"  # Se precisar do título, remova o comentário
@@ -62,5 +89,3 @@ p22 <- ggplot2::ggplot(ne_m1, ggplot2::aes(x = count_n, color = NE, group = NE))
     axis.title.y = ggplot2::element_text(size = 22, angle = 90)
   )
 
-  #ggplot(ne_m1, aes(x = count_n, color = NE)) +
-  #  stat_ecdf(geom = "line", linewidth = 1)
